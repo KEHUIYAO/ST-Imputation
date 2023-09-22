aa4590af79c5df1ed4fab85042c60eaa31bb7847
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-09-22 18:11:50 -0500

    update

--- data/soil_moisture_sparse.py
+++ data/soil_moisture_sparse.py
@@ -51,7 +51,7 @@ class SoilMoistureSparse(PandasDataset, MissingValuesMixin):
 
         y = df.pivot(index='Date', columns='POINTID', values='SMAP_1km')
 
-        covariates = ['prcp', 'srad', 'tmax', 'tmin', 'vp', 'SMAP_36km']
+        covariates = ['prcp', 'srad', 'tmax', 'tmin', 'vp']
         X = []
         for cov in covariates:
             x = df.pivot(index='Date', columns='POINTID', values=cov).values
