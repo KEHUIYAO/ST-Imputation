64341124c7066fccb5540801a5bb3b807ef81e67
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-09-26 15:45:24 -0500

    update

--- data/soil_moisture_sparse.py
+++ data/soil_moisture_sparse.py
@@ -26,17 +26,17 @@ class SoilMoistureSparse(PandasDataset, MissingValuesMixin):
 
     def __init__(self, mode='train', seed=42):
         df, dist, mask, st_coords_new, temporal_encoding, X_new = self.load(mode=mode)
-        # super().__init__(dataframe=df,
-        #                  similarity_score="distance",
-        #                  mask=mask,
-        #                  attributes=dict(dist=dist, temporal_encoding=temporal_encoding,
-        #                   st_coords=st_coords_new, covariates=X_new))
-
         super().__init__(dataframe=df,
                          similarity_score="distance",
                          mask=mask,
                          attributes=dict(dist=dist, temporal_encoding=temporal_encoding,
-                          st_coords=st_coords_new))
+                          st_coords=st_coords_new, covariates=X_new))
+
+        # super().__init__(dataframe=df,
+        #                  similarity_score="distance",
+        #                  mask=mask,
+        #                  attributes=dict(dist=dist, temporal_encoding=temporal_encoding,
+        #                   st_coords=st_coords_new))
 
         rng = np.random.RandomState(seed)
 
