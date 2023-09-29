4a7bfef61e1fd07f870533a037869887511727f8
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-09-28 20:23:05 -0500

    update

--- spin/imputers/csdi_imputer.py
+++ spin/imputers/csdi_imputer.py
@@ -182,6 +182,7 @@ class CsdiImputer(Imputer):
         # self.val_metrics.update(epsilon_hat, epsilon, batch.eval_mask)
         # self.log_metrics(self.val_metrics, batch_size=batch.batch_size)
         # self.log_loss('val', val_loss, batch_size=batch.batch_size)
+        print(val_loss_sum)
         self.log('test_mse', val_loss_sum, on_step=True, on_epoch=True, prog_bar=True)
 
 
@@ -235,7 +236,7 @@ class CsdiImputer(Imputer):
         # Logging
         self.test_metrics.update(y_hat.detach(), y, eval_mask)
         self.log_metrics(self.test_metrics, batch_size=batch.batch_size)
-        self.log_loss('test', test_loss, batch_size=batch.batch_size)
+        # self.log_loss('test', test_loss, batch_size=batch.batch_size)
         return test_loss
 
     def predict_step(self, batch, batch_idx, dataloader_idx=None):
