ccde9cb5d83d2743e32e2d56e5b79aa692050069
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-09-29 00:15:48 -0500

    update

--- experiments/run_imputation.py
+++ experiments/run_imputation.py
@@ -41,7 +41,7 @@ def parse_args():
     #parser.add_argument("--model-name", type=str, default='interpolation')
     parser.add_argument("--dataset-name", type=str, default='soil_moisture_sparse_point')
     #parser.add_argument("--config", type=str, default=None)
-    parser.add_argument("--config", type=str, default='imputation/csdi_soil_moisture.yaml')
+    parser.add_argument("--config", type=str, default=None)
     parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--check-val-every-n-epoch', type=int, default=1)
     parser.add_argument('--batch-inference', type=int, default=32)
@@ -383,8 +383,8 @@ def run_experiment(args):
     ########################################
     # testing                              #
     ########################################
-
-    imputer.load_model(checkpoint_callback.best_model_path)
+    if require_training:
+        imputer.load_model(checkpoint_callback.best_model_path)
     imputer.freeze()
     trainer.test(imputer, dataloaders=dm.test_dataloader(
         batch_size=args.batch_inference))
