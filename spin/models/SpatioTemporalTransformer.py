5e8150e734953bd366988643b044e6ce75192327
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-10-05 10:08:35 -0500

    update

--- spin/models/SpatioTemporalTransformer.py
+++ spin/models/SpatioTemporalTransformer.py
@@ -33,7 +33,7 @@ def get_torch_trans(heads=8, layers=1, channels=64):
 
 def Conv1d_with_init(in_channels, out_channels, kernel_size):
     layer = nn.Conv1d(in_channels, out_channels, kernel_size)
-    nn.init.kaiming_normal_(layer.weight)
+    #nn.init.kaiming_normal_(layer.weight)
     return layer
 
 
