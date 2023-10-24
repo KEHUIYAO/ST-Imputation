396f4238750c477622ca5ec85aab272a02f1efb3
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-10-23 22:15:38 -0500

    update

--- data/soil_moisture_sparse.py
+++ data/soil_moisture_sparse.py
@@ -138,12 +138,12 @@ class SoilMoistureSparse(PandasDataset, MissingValuesMixin):
 
         X = np.stack(X, axis=-1)
 
-        # static features
-        static_features = ['elevation', 'slope', 'aspect', 'hillshade', 'clay', 'sand', 'bd', 'soc', 'LC']
-        tmp = pd.read_csv(os.path.join(current_dir, 'constant_grid.csv'))
-        tmp = tmp.iloc[:, 4:].values  # (K, C)
-        tmp = np.tile(tmp[np.newaxis, :, :], (X.shape[0], 1, 1))
-        X = np.concatenate([X, tmp], axis=-1)
+        # # static features
+        # static_features = ['elevation', 'slope', 'aspect', 'hillshade', 'clay', 'sand', 'bd', 'soc', 'LC']
+        # tmp = pd.read_csv(os.path.join(current_dir, 'constant_grid.csv'))
+        # tmp = tmp.iloc[:, 4:].values  # (K, C)
+        # tmp = np.tile(tmp[np.newaxis, :, :], (X.shape[0], 1, 1))
+        # X = np.concatenate([X, tmp], axis=-1)
 
 
 
