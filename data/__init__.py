6390438e7ea2397911e5e6cc69f344bcce8c53bb
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-11-07 00:55:34 -0600

    update

--- data/__init__.py
+++ data/__init__.py
@@ -3,3 +3,4 @@ from .descriptive_st import DescriptiveST
 from .dynamic_st import DynamicST
 from .soil_moisture import SoilMoisture
 from .soil_moisture_sparse import SoilMoistureSparse
+from .soil_moisture_hb import SoilMoistureHB
