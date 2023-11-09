77b4ccf63ecf5ea7911478fb9792bbaf4e511a1a
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2023-11-09 16:46:36 -0600

    update

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,6 @@
 from .gp import GaussianProcess
 from .descriptive_st import DescriptiveST
 from .dynamic_st import DynamicST
-from .soil_moisture import SoilMoisture
 from .soil_moisture_sparse import SoilMoistureSparse
 from .soil_moisture_hb import SoilMoistureHB
 from .healing_mnist import HealingMnist
